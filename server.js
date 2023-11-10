b73b652e9afa41f0d9321ffb0313415935ffc2c0
Larwive <larwive@gmail.com>
2023-11-10 20:39:04 +0100

    Include node_modules

--- server.js
+++ server.js
@@ -2,26 +2,19 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
 const cors = require('cors');
-
-// Enable CORS for all routes
-app.use(cors());
-
-app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
-
-// app.js
 const postgres = require('postgres');
 require('dotenv').config();
 
-let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
-PGUSER = decodeURIComponent(PGUSER);
+const corsOptions = {
+    origin: 'https://larwive.github.io',
+};
+
+// Enable CORS for all routes
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
 
 const sql = postgres({
     host: PGHOST,
@@ -36,21 +29,26 @@ const sql = postgres({
 });
 
 async function getPgVersion() {
-    const result = await sql`select version()`;
-    console.log(result);
+    try {
+        const result = await sql`select version()`;
+        console.log(result);
+    } catch (err) {
+        console.error('Error connecting to PostgreSQL:', err);
+    }
 }
 
 getPgVersion();
 
-
 app.get('/api/data', async (req, res) => {
     try {
-        const client = await pool.connect();
-        const result = await client.query('SELECT * FROM your_table');
-        res.json(result.rows);
-        client.release();
+        const result = await sql`SELECT * FROM your_table`;
+        res.json(result);
     } catch (err) {
         console.error('Error executing query', err);
         res.status(500).json({ error: 'An error occurred' });
     }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
