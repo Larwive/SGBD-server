1b87747258334fcc8945f3cfc32ba570b0b9d909
Larwive <larwive@gmail.com>
2023-11-13 21:21:20 +0100

    Testing Glitch auto update.

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.route('/api/data')
         try {
             const result = await client.query(
                 "SELECT $1::text as message", [
-                    "Hello world from node.js server ! Hey. broootttttterdsd",
+                    "Hello world from node.js server ! Hey. broootttttterdsder ?",
                 ])
             ;
             res.json(result);
