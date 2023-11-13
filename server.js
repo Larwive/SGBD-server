fdcf5e08a42830b8b4a00f5a72e51192d4f59ed2
Larwive <larwive@gmail.com>
2023-11-13 21:32:39 +0100

    Testing Glitch auto update.

--- server.js
+++ server.js
@@ -20,10 +20,10 @@ app.use(express.json());
 
 // Database connection
 const client = new Client({
-    user: process.env.USER,
-    host: process.env.HOST,
-    database: process.env.DATABASE,
-    password: process.env.PASSWORD,
+    user: `${process.env.USER}`,
+    host: `${process.env.HOST}`,
+    database: `${process.env.DATABASE}`,
+    password: `${process.env.PASSWORD}`,
     port: process.env.PORT,
 });
 
