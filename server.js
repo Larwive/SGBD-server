0a32c7c099bdb20fe44f05b7db6facf8a4ac30b5
Larwive <larwive@gmail.com>
2023-11-15 20:25:33 +0100

    Ajout route `/intervention` pour obtenir les interventions entre deux dates.

--- server.js
+++ server.js
@@ -82,6 +82,24 @@ app.post('/users', async (req, res) => {
         console.error('Error getting the clients table\'s data.', err);
     }})
 
+app.post('/intervention', async (req, res) => {
+    const postData = req.body;
+
+    console.log(`Getting interventions betweens ${postData.postMinDate} and ${postData.postMaxDate}.\n`);
+    const minDate = postData.postMinDate === null ? 'true' : `date_prise_en_charge >= TO_DATE(${postData.postMinDate}, 'DD/MM/YYYY')`;
+    const maxDate = postData.postMaxDate === null ? 'true' : `date_prise_en_charge <= TO_DATE(${postData.postMaxDate}, 'DD/MM/YYYY')`;
+    try {
+        const countQuery = await client.query(`SELECT COUNT(*) FROM interventions WHERE ${minDate} AND ${maxDate};`);
+        const rowsQuery = await client.query(`SELECT * FROM interventions WHERE ${minDate} AND ${maxDate};`);
+        res.json({
+            count: countQuery.rows[0].count,
+            rows: rowsQuery.rows
+        });
+    } catch (err) {
+        console.error('Error getting the interventions table\'s data.', err);
+        res.status(500).json({error: 'An error occurred (POST)'});
+    }})
+
 //Auto github update
 app.post('/git', (req, res) => {
     let hmac = crypto.createHmac("sha1", process.env.SECRET);
