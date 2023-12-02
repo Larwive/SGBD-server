85f1ee8ab405bdf6f4bfc87b2e17fddbebece5f8
Larwive <larwive@gmail.com>
2023-12-02 17:38:15 +0100

    Update git.ts

--- routes/git.ts
+++ routes/git.ts
@@ -5,20 +5,20 @@ import {app} from "../server";
 //Auto GitHub update
 export function route_git(){
     app.post("/git", (req, res) => {
-        let hmac!: crypto.Hmac;
-        if (process.env.SECRET !== undefined) {
-            hmac = crypto.createHmac("sha1", process.env.SECRET);
-        }
-        let sig: string =
-            "sha1=" + hmac.update(JSON.stringify(req.body)).digest("hex");
+        //let hmac!: crypto.Hmac;
+        //if (process.env.SECRET !== undefined) {
+        //    hmac = crypto.createHmac("sha1", process.env.SECRET);
+        //}
+        //let sig: string =
+        //    "sha1=" + hmac.update(JSON.stringify(req.body)).digest("hex");
 
         // If event is "push"
-        if (
-            req.headers["x-github-event"] === "push" &&
-            sig === req.headers["x-hub-signature"]
-        ) {
+        //if (
+            //req.headers["x-github-event"] === "push" &&
+            //sig === req.headers["x-hub-signature"]
+        //) {
             console.log("Push incoming...");
-            if (req.headers["x-github-event"] === "push") {
+            //if (req.headers["x-github-event"] === "push") {
                 cmd.run("chmod 777 git.sh", (err: any, data: string) => {
                     if (data) console.log(data);
                     if (err) console.log(err);
@@ -45,8 +45,8 @@ export function route_git(){
                             .join("\n");
                 console.log(
                     `> [GIT] Updated with origin/main\n         Latest commit: ${commits}`);
-            }
-        }
+            //}
+        //}
         return res.status(200).json({ success: "Webhook received successfully." });
 });
 }
